3fafb6eebf5bf758629c792708d00a820e73fe2c
Michael Naraiswar <naraiswar@gmail.com>
2019-03-07 09:15:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,27 @@ end
 def array_with_two_elements
   arr = [1, 2]
 end
+
+def my_first_element (arr)
+  arr[0]
+end
+
+def my_third_element (arr)
+  arr[2]
+end
+
+def my_last_element (arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods (arr)
+  arr.first
+end
+
+def last_element_with_array_methods (arr)
+  arr.last
+end
+
+def length_of_array (arr)
+  arr.length
+end
